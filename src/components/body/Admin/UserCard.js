b0eb7110f818b81e6d86830ed9a7bb2cb0b3e55e
lasteik <lastevilday@gmail.com>
2023-05-11 12:07:36 +0500

    restructure send request

--- src/components/body/Admin/UserCard.js
+++ src/components/body/Admin/UserCard.js
@@ -1,17 +1,51 @@
+import { useState } from "react";
 import "./UserCard.css";
 function UserCard(props) {
+  var [isChange, setIsChange] = useState(false);
+
+  function ActiveChangeUserData(event) {
+    event.preventDefault();
+    setIsChange(true);
+  }
+  function ChangeUserData(event) {
+    event.preventDefault();
+  }
+  //ToDo: Сделать перенос строк для мобильного вида.
   return (
-    <div className="user-card-body">
-      <div className="user-info">
-        <div>{props.user.firstName}</div>
-        <div>{props.user.lastName}</div>
-        <div>{props.user.departmentTitle}</div>
-        <div>{props.user.email}</div>
-      </div>
-      <div className="user-btn">
-        <button className="change-btn">Изменить</button>
-        <button className="activation-btn">Активировать</button>
-      </div>
+    <div>
+      {isChange === false && (
+        <div className="user-card-body">
+          <div className="user-info">
+            <div>{props.user.firstName}</div>
+            <div>{props.user.lastName}</div>
+            <div>{props.user.departmentTitle}</div>
+            <div>{props.user.email}</div>
+          </div>
+          <div className="user-btn">
+            <button onClick={ActiveChangeUserData} className="change-btn">
+              Изменить
+            </button>
+          </div>
+        </div>
+      )}
+
+      {isChange === true && (
+        <div className="user-card-body">
+          <div className="user-info">
+            <input value={props.user.firstName} />
+            <input value={props.user.lastName} />
+            <input value={props.user.email} />
+            <input value={props.user.lastName} />
+            <div>{props.user.departmentTitle}</div>
+            <div>{}</div>
+          </div>
+          <div className="user-btn">
+            <button onClick={ChangeUserData} className="activation-btn">
+              Активировать
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
